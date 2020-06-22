9be124c8e145b42b77950b9699307aa61675ba30
sxj415324@163.com <sxj415324@163.com>
2020-06-22 21:41:52 +0800

    should_return_Negative_3_when_success_0_times_and_fail_1_times

--- src/test/java/com/twschool/practice/domain/CalculateScoreTest.java
+++ src/test/java/com/twschool/practice/domain/CalculateScoreTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CalculateScoreTest {
     int userTotalScores;
     @Test
-    public void should_return_3_when_success_1times_and_fail_0times() {
+    public void should_return_3_when_success_1_times_and_fail_0_times() {
         //given
         CalculateScore calculateScore = new CalculateScore(0,1);
         //when
@@ -16,7 +16,7 @@ public class CalculateScoreTest {
     }
 
     @Test
-    public void should_return_0_when_success_2times_and_fail_2times() {
+    public void should_return_0_when_success_2_times_and_fail_2_times() {
         //given
         CalculateScore calculateScore = new CalculateScore(2,2);
         //when
@@ -26,7 +26,7 @@ public class CalculateScoreTest {
     }
 
     @Test
-    public void should_return_11_when_success_3times_and_fail_0times() {
+    public void should_return_11_when_success_3_times_and_fail_0_times() {
         //given
         CalculateScore calculateScore = new CalculateScore(0,3);
         //when
@@ -36,7 +36,7 @@ public class CalculateScoreTest {
     }
 
     @Test
-    public void should_return_20_when_success_5times_and_fail_0times() {
+    public void should_return_20_when_success_5_times_and_fail_0_times() {
         //given
         CalculateScore calculateScore = new CalculateScore(0,5);
         //when
@@ -46,13 +46,23 @@ public class CalculateScoreTest {
     }
 
     @Test
-    public void should_return_25_when_success_6times_and_fail_0times() {
+    public void should_return_25_when_success_6_times_and_fail_0_times() {
         //given
         CalculateScore calculateScore = new CalculateScore(0,6);
         //when
         userTotalScores =calculateScore.calculate() + calculateScore.calculateExtra();
         //then
         Assert.assertEquals(25, userTotalScores);
+    }
+
+    @Test
+    public void should_return_Negative_3_when_success_0_times_and_fail_1_times() {
+        //given
+        CalculateScore calculateScore = new CalculateScore(1,0);
+        //when
+        userTotalScores =calculateScore.calculate() + calculateScore.calculateExtra();
+        //then
+        Assert.assertEquals(-3, userTotalScores);
     }
 
 }
